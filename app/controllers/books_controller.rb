8b44fc993b1d68a6032e8e07f562fa6ae62d939b
takokke <138221752+takokke@users.noreply.github.com>
2023-09-22 09:42:42 +0000

    バリデーションのエラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,18 +2,39 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    
   end
-  
-  def new
-  end
-  
+
   def create
+    @book = Book.new(book_parmas)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
     
   end
 
   def edit
   end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  def book_parmas
+    params.require(:book).permit(:title,:body)
+  end
+  
 end
