710e4ba1021ccaf52f585b52c3e6e135f1034514
takokke <138221752+takokke@users.noreply.github.com>
2023-09-22 08:14:44 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+    
+  end
+
+  def edit
+  end
+end
